26500cd4978a4543886cff0eefd3a6ddc49d7c0d
maximbelo <mbelomestnov@hotmail.ca>
2022-01-14 15:31:20 -0500

    Routing

--- FirstApp/index.js
+++ FirstApp/index.js
@@ -4,8 +4,36 @@ const express = require("express");
 const app = express();
 
 // Anytime we have an incoming request, this callback will run
-app.use(() => {
-  console.log("We got a new request!");
+// Gives us access to two params -incoming req and outgoing res
+// app.use((req, res) => {
+//   console.log("We got a new request!");
+//   res.send("<h1> This is my webpage! </h1>");
+// });
+
+// Routing
+// cats => meow
+// dogs => woof!
+// / => home (usually refered to as the root route)
+
+app.get("/", (req, res) => {
+  res.send("Welcome home!");
+});
+
+app.post("./cats", (req, res) => {
+  res.send("POST REQUEST TO /CATS! THIS IS DIFFERENT THAN A GET REQUEST");
+});
+
+app.get("/cats", (req, res) => {
+  // res.send() is used to send back content
+  res.send("MEOW!");
+});
+
+app.get("/dogs", (req, res) => {
+  res.send("WOOF!");
+});
+
+app.get("*", (req, res) => {
+  res.send("I don't know that path!");
 });
 
 // Need to go to localhost:3000 in our browser (or any other address that we give to our server) to get to the local server
